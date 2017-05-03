edc8c9d6ddbc689e621871474c13e35a8c449d44
Baymax <dolphineor@gmail.com>
2017-05-03 15:45:04 +0800

    Update

--- ui/src/components/HelloFromVux.vue
+++ ui/src/components/HelloFromVux.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <div class="vux-demo">
-      <img class="logo" src="../assets/vux_logo.png">
-      <h1> </h1>
+      <img class="logo" src="../assets/vux_logo.png" @click="greet">
+      <h1>Squirrel</h1>
     </div>
     <group title="cell demo">
       <cell title="Vux" value="Cool" is-link>
@@ -13,31 +13,41 @@
 </template>
 
 <script>
-import { Group, Cell } from 'vux'
+  import {Group, Cell} from 'vux'
 
-export default {
-  components: {
-    Group,
-    Cell
-  },
-  data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello World!'
+  export default {
+    components: {
+      Group,
+      Cell
+    },
+    data () {
+      return {
+        // note: changing this line won't causes changes
+        // with hot-reload because the reloaded component
+        // preserves its current state and we are modifying
+        // its initial state.
+        msg: 'Hello World!'
+      }
+    },
+    methods: {
+      greet: function () {
+        this.$vux.toast.show({
+          text: 'Loading',
+          time: 1500,
+          isShowMask: true
+        })
+      }
     }
   }
-}
 </script>
 
 <style>
-.vux-demo {
-  text-align: center;
-}
-.logo {
-  width: 100px;
-  height: 100px
-}
+  .vux-demo {
+    text-align: center;
+  }
+
+  .logo {
+    width: 100px;
+    height: 100px
+  }
 </style>
