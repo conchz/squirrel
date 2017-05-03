ad45471d4524eafc9106a5f37aa3c341091dc74b
Baymax <dolphineor@gmail.com>
2017-05-03 10:01:49 +0800

    Update redis_test.go

--- app/redis_test.go
+++ app/redis_test.go
@@ -11,19 +11,13 @@ func TestConnectToRedis(t *testing.T) {
 	config := LoadConfig()
 	client := ConnectToRedis(config)
 
-	defer closeRedisClient(client)
-
-	fmt.Println(client.Info())
-}
-
-func closeRedisClient(client *redis.Client) {
-	if client != nil {
+	defer func(client *redis.Client) {
 		fmt.Println("Closing Redis client...")
 		err := client.Close()
 		if err != nil {
 			panic(errors.New("Closing Redis client failed!"))
 		}
-	} else {
-		panic(errors.New("There is no available Redis client!"))
-	}
+	}(client)
+
+	fmt.Println(client.Info())
 }
